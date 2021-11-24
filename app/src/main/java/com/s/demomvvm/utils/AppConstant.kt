0ba87a92b2733fddc675d68c109fb033dde4e55d
Nguyen Luc <nguyenluc1232@gmail.com>
2021-11-24 22:33:08 +0900

    remove key

--- app/src/main/java/com/s/demomvvm/utils/AppConstant.kt
+++ app/src/main/java/com/s/demomvvm/utils/AppConstant.kt
@@ -7,8 +7,6 @@ package com.s.demomvvm.utils
 class AppConstant {
     companion object {
         val PREFERENCE_NAME = "demo_pref"
-        val BASE_URL =  "covid-19-coronavirus-statistics2.p.rapidapi.com"
-        val API_KEY  = "a0ad7ea486msh817538db44bf197p1519f6jsn524f3b30aaf1"
-        val API_HOST = "covid-19-coronavirus-statistics2.p.rapidapi.com"
+        
     }
 }
